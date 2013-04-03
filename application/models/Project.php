590b4cfb3a1362e31a34b8c75e8b8da335827372
Thiago Soares <tcsoares84@gmail.com>
2013-04-03 18:21:42 -0300

    Addition of Project Model and Project DBTable.

---
+++ application/models/Project.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The Project Model class is used to make custom queries.
+ *
+ * @category  Model
+ * @access    public
+ * @package   TechForTi
+ * @version   0.1
+ * @author    Thiago Soares <tcsoares84@gmail.com>
+ * @see       http://www.techforti.com.br
+ * @copyright Copyright (c) Tech For Soluções em TI
+ */
+class Application_Model_Project
+{
+    /**
+     * Return all project list.
+     *
+     * @access public
+     * @return array
+     */
+    public function getProjects()
+    {
+        
+    }
+}
